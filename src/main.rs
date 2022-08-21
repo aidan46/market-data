8f2b73e0d2d1dda66884ea41b922597fb954366d
aidan <aidansullivan.dev@gmail.com>
2022-08-21 16:23:24 +0200

    Call all functions in main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use binance_md::Client;
+use binance_md::{market_data::Interval, Client};
 use dotenv::dotenv;
 use std::env;
 
@@ -8,9 +8,29 @@ async fn main() -> Result<()> {
     dotenv()?;
     let base_url = env::var("ENDPOINT")?;
     let client = Client::new(base_url);
+    let symbol = "BTCUSDT";
+    let limit = Some(1);
+    let from_id = None;
+    let start_time = None;
+    let end_time = None;
+    let interval = Interval::Minute(1);
+    client.ping().await;
+    println!("{:#?}", client.time().await?);
+    println!("{:#?}", client.exchange_info(Some(&[symbol])).await?);
+    println!("{:#?}", client.depth(symbol, limit).await?);
+    println!("{:#?}", client.trades(symbol, limit).await?);
     println!(
         "{:#?}",
-        client.exchange_info(Some(&["BTCUSDT", "BNBBTC"])).await?
+        client
+            .agg_trades(symbol, from_id, start_time, end_time, limit)
+            .await?
     );
+    println!(
+        "{:#?}",
+        client
+            .kline(symbol, interval, start_time, end_time, limit)
+            .await?
+    );
+    println!("{:#?}", client.avg_price(symbol).await?);
     Ok(())
 }
